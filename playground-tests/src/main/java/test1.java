e6a5e201f89919cd49cbbce3da60c87533378dba
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2018-03-20 20:24:30 +0100

    test1 - running and quiting the browser

---
+++ playground-tests/src/main/java/test1.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+  public class test1 {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+      driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      baseUrl = "https://www.wp.pl/";
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testFirst() throws Exception {
+      driver.get(baseUrl + "/");
+      driver.quit();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+      driver.quit();
+      String verificationErrorString = verificationErrors.toString();
+      if (!"".equals(verificationErrorString)) {
+        fail(verificationErrorString);
+      }
+    }
+
+  }
+
+
