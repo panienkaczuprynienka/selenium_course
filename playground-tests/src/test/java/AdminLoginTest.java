ec182d0b3e60cf7222b98f6f1a33a1cdd721f94c
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2018-03-27 16:37:03 +0200

    running FF with old schema / new schema

--- playground-tests/src/test/java/AdminLoginTest.java
+++ playground-tests/src/test/java/AdminLoginTest.java
@@ -1,56 +1,69 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-  public class AdminLoginTest {
-    WebDriver wd;
-    String browser= "firefox";
+public class AdminLoginTest {
+  WebDriver wd;
+  WebDriverWait wait;
+  String browser = "firefox";
+  String runningSchemaForFF = "new";
 
 
-    @BeforeMethod
-    public void setUp() throws Exception {
-      if (browser.equals("firefox")) {
+  @BeforeMethod
+  public void setUp() throws Exception {
+    if (browser.equals("firefox")) {
+      if (runningSchemaForFF.equals("old")) {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        wd = new FirefoxDriver(caps);
+        System.out.println(((HasCapabilities) wd).getCapabilities());
+        wait = new WebDriverWait(wd, 10);
+      } else if (runningSchemaForFF.equals("new")) {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-      } else if (browser.equals("chrome")) {
-        wd = new ChromeDriver();
-      } else if (browser.equals("IE")) {
-        wd = new InternetExplorerDriver();
       }
-      wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    } else if (browser.equals("chrome")) {
+      wd = new ChromeDriver();
+    } else if (browser.equals("IE")) {
+      wd = new InternetExplorerDriver();
     }
-
-    @Test
-    public void login() {
-      wd.get("http://localhost:8080/litecart/admin/login.php");
-      wd.findElement(By.name("username")).sendKeys("admin");
-      wd.findElement(By.name("password")).sendKeys("admin");
-      wd.findElement(By.name("login")).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-      wd.quit();
-    }
-
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-      try {
-        wd.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
-
-
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
   }
+
+  @Test
+  public void login() {
+    wd.get("http://localhost:8080/litecart/admin/login.php");
+    wd.findElement(By.name("username")).sendKeys("admin");
+    wd.findElement(By.name("password")).sendKeys("admin");
+    wd.findElement(By.name("login")).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+
+}
 
 
